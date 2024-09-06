bea8662a0f59f490c206ea91f9e35bd598eee701
KaiqueMaia0 <155084396+KaiqueMaia0@users.noreply.github.com>
2024-09-06 20:44:34 -0300

    agr foi

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function validarSenha() {
   const senha = document.querySelector(input[id=senha]);
   const csenha = document.querySelector(input[id=csenha]);
-  if (confirma.value === senha.value){
-  confirma.setCustomValidity('');
+  if (csenha.value === senha.value){
+  csenha.setCustomValidity('');
   } else {
       alert("Senhas não condizem!")
   }
-  function formenviado(){
-      alert("Formulário enviado!")
-  }
+}
+function formenviado(){
+  alert("Formulário enviado!")
 }
