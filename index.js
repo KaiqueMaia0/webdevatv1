b8b797ba0a2c0077b3fc162eb9e55af545f7289e
KaiqueMaia0 <155084396+KaiqueMaia0@users.noreply.github.com>
2024-09-06 14:31:21 -0300

    parece que agora vai

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function validarSenha() {
   const senha = document.querySelector(input[id=senha]);
   const csenha = document.querySelector(input[id=csenha]);
-  if (confirma.value == senha.value){
-  confirma.setCustomValidity('');
-  } else {
-   confirm.setCustomValidity('As senhas não são iguais!');
-  }
+    if (confirma.value === senha.value){
+    confirma.setCustomValidity('');
+    } else {
+    confirm.setCustomValidity('As senhas não são iguais!');
+    }
 }
