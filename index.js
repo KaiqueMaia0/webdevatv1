68d40ea114772970bd6284576f888b43950c39f6
KaiqueMaia0 <155084396+KaiqueMaia0@users.noreply.github.com>
2024-09-06 14:36:44 -0300

    se não for agr eu desisto

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function validarSenha() {
   const senha = document.querySelector(input[id=senha]);
   const csenha = document.querySelector(input[id=csenha]);
-    if (confirma.value === senha.value){
-    confirma.setCustomValidity('');
-    } else {
-    confirm.setCustomValidity('As senhas não são iguais!');
-    }
+  if (confirma.value === senha.value){
+  confirma.setCustomValidity('');
+  } else {
+   confirma.setCustomValidity('As senhas não são iguais!');
+  }
+  function formenviado(){
+      alert("Formulário enviado!")
+  }
 }
